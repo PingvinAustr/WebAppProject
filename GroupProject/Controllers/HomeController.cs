cd8fda7f846d0fa4858d14fe585a65633c3f6d7e
PingvinAustr <yarik.chushenko1@gmail.com>
2021-11-23 23:37:45 +0200

    Добавьте файлы проекта.

---
+++ GroupProject/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows;
+namespace GroupProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //create data context
+        rubricsEntities db = new rubricsEntities();
+
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+
+            //CODE --> FORM
+            ViewBag.UserID = id;
+            ViewData["Message"] = "HI MIR";
+            return View();
+
+        }
+        
+        [HttpPost]
+        public string Buy()
+        {
+            //FORM --> CODE 
+
+            // добавляем информацию о покупке в базу данных
+            //db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            //db.SaveChanges();
+
+           
+            return "Спасибо," +   ", за покупку!";
+        }
+        
+
+
+
+
+        public ActionResult Index()
+        {
+            //Get all users from DB
+            IEnumerable<Users> users = db.Users;
+
+            //transfer all users to ViewBag
+            ViewBag.Users = users;
+
+            // return view
+            return View();
+        }
+
+
+    }
+}
