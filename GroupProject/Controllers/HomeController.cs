ccfcf64ebe467590bc9df6ec2905b1d3117630b8
PingvinAustr <yarik.chushenko1@gmail.com>
2021-11-25 00:56:47 +0200

    Настроил редирект,начал работу над страницей обычного пользователя

--- GroupProject/Controllers/HomeController.cs
+++ GroupProject/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Windows;
+using GroupProject.Models;
+
 namespace GroupProject.Controllers
 {
     public class HomeController : Controller
@@ -13,19 +15,20 @@ namespace GroupProject.Controllers
         rubricsEntities db = new rubricsEntities();
 
 
-        [HttpGet]
-        public ActionResult Buy(int id)
+        //[HttpGet]
+        /*
+        public ActionResult Signin(string test)
         {
 
             //CODE --> FORM
-            ViewBag.UserID = id;
+            ViewBag.UserID = 1;
             ViewData["Message"] = "HI MIR";
             return View();
 
         }
         
         [HttpPost]
-        public string Buy()
+        public string Signin(List<string> names)
         {
             //FORM --> CODE 
 
@@ -35,13 +38,17 @@ namespace GroupProject.Controllers
             //db.SaveChanges();
 
            
-            return "Спасибо," +   ", за покупку!";
+            return "Спасибо," +  names.Count+ ", за покупку!";
         }
-        
+        */
+
+        int UserIDSaved = 0, UserRoleIDSaved=0;
 
 
+       
 
 
+        [HttpGet]
         public ActionResult Index()
         {
             //Get all users from DB
@@ -50,9 +57,65 @@ namespace GroupProject.Controllers
             //transfer all users to ViewBag
             ViewBag.Users = users;
 
+            //Отобразится при инициализации страницы
+            // ViewBag.newvar = "init";
             // return view
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult Index(string Input1, string Input2)
+        {
+            //Get all users from DB
+            IEnumerable<Users> users = db.Users;
+
+            //transfer all users to ViewBag
+            ViewBag.Users = users;
+
+
+            //Валидация двух полей ввода
+            if (Input1 == "") ViewBag.validation_input1 = "*обов'язкове поле";
+            if (Input2 == "") ViewBag.validation_input2 = "*обов'язкове поле";
+
+            //Если одно из полей пустое, а второе - нет, то мы НЕ должны удалять контент уже заполненного поля
+            if ((Input1 == "") && (Input2 != "")) ViewBag.to_save_second_input = Input2;
+            if ((Input1 != "") && (Input2 == "")) ViewBag.to_save_first_input = Input1;
+
+            
+            int k = 0;
+            //Если оба поля заполнены - проверяем правильность пароля и логина
+            if ((Input1 != "") && (Input2 != ""))
+            {
+               
+                var user_list = db.Users.ToList();
+                foreach (Users user in user_list)
+                {
+                    if ((user.UserPassword == Input2) && (user.UserName == Input1))
+                    {
+                        UserIDSaved = user.UserID;
+                        UserRoleIDSaved = int.Parse(user.UserRoleID.ToString());
+
+                        k++;
+                    }
+                }
+                //Если не нашлось соответствия Логин+Пароль введенных пользователем - выдаем сообщение об ошибке
+                if (k == 0) { ViewBag.validation_input1 = "Неправильний логін або пароль!"; }
+             
+
+            }
+
+            ViewBag.transfer_user_id = UserIDSaved;
+
+
+            // return view
+           if (k==0) return View();
+           
+           if ((k!=0)&&(UserRoleIDSaved==3)) return View("~/Views/User/Index.cshtml");
+            return View();
+        }
+
+      
 
 
     }
---
+++ GroupProject/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows;
+using GroupProject.Models;
+
+
+namespace GroupProject.Controllers
+{
+    public class UserController : Controller
+    {
+
+        // GET: Item
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+            //Response.Write("[" + ViewBag.Abobus + "]");
+            return View();
+        }
+
+
+        public void Aboba()
+        {
+            Response.Write("re");
+        }
+
+
+        public void Execute(RequestContext requestContext)
+        {
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            var response = requestContext.HttpContext.Response;
+            //response.Write("<h2>Ваш IP-адрес: " + ip + "</h2>");
+           
+        }
+    }
+}
